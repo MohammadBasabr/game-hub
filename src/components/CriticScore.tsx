05cf2c94069267286ff9b3ddc64ad2eda440b48d
Mohammad Baasabr <m.basabr75@gmail.com>
2023-04-04 01:50:48 +0330

    Displaying critic score.

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,12 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return <Badge colorScheme={color} fontSize='14px' paddingX={2}>{score}</Badge>;
+};
+
+export default CriticScore;
